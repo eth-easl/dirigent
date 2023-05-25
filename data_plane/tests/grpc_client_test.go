e7a6fbd345649d5409c41209f9f4c09c1609efa6
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2023-05-25 10:34:02 +0200

    100 invocations test
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- data_plane/tests/grpc_client_test.go
+++ data_plane/tests/grpc_client_test.go
@@ -1,12 +1,37 @@
 package tests
 
 import (
+	"github.com/sirupsen/logrus"
 	"testing"
 )
 
 func TestInvocationProxying(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+
 	err := FireInvocation(t, "localhost", "8080")
 	if err != nil {
 		t.Error("Invocation failed.")
+	}
+}
+
+func Test_100Invocations(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+
+	invocationCount := 100
+	ch := make(chan struct{})
+
+	for i := 0; i < invocationCount; i++ {
+		go func() {
+			err := FireInvocation(t, "localhost", "8080")
+			if err != nil {
+				t.Error("Invocation failed.")
+			}
+
+			ch <- struct{}{}
+		}()
+	}
+
+	for i := 0; i < invocationCount; i++ {
+		<-ch
 	}
 }
