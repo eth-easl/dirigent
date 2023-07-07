a0b6f4c5ab748378b8efcefb7002f820fa8135f3
François Costa <frankost.costa@gmail.com>
2023-07-07 14:11:31 +0200

    Improve synchronization data structure

---
+++ utils/atomic_map_counter.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"sync"
+	"sync/atomic"
+
+	"github.com/sirupsen/logrus"
+)
+
+type AtomicMap[T any] struct {
+	m sync.Map
+}
+
+func NewAtomicMap[T any]() AtomicMap[T] {
+	return AtomicMap[T]{}
+}
+
+func (c *AtomicMap[T]) AtomicGet(key T) int64 {
+	count, ok := c.m.Load(key)
+	if ok {
+		val, ok := count.(*int64)
+		if !ok {
+			logrus.Fatal("Type assertion failed")
+		}
+
+		return atomic.LoadInt64(val)
+	}
+
+	return 0
+}
+
+func (c *AtomicMap[T]) AtomicAdd(key T, value int64) int64 {
+	count, loaded := c.m.LoadOrStore(key, &value)
+	if loaded {
+		val, ok := count.(*int64)
+		if !ok {
+			logrus.Fatal("Type assertion failed")
+		}
+
+		return atomic.AddInt64(val, value)
+	}
+
+	val, ok := count.(*int64)
+	if !ok {
+		logrus.Fatal("Type assertion failed")
+	}
+
+	return *val
+}
+
+func (c *AtomicMap[T]) AtomicIncrement(key T) {
+	c.AtomicAdd(key, 1)
+}
+
+func (c *AtomicMap[T]) AtomicDecrement(key T) {
+	c.AtomicAdd(key, -1)
+}
+
+func (c *AtomicMap[T]) AtomicDelete(key T) {
+	c.m.Delete(key)
+}
