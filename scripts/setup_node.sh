c48b66b147064886245622b7b6ad04528151f865
François Costa <frankost.costa@gmail.com>
2023-10-21 16:10:41 +0200

    Add systemd files

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -37,3 +37,6 @@ curl -L ${release_url}/download/${latest}/firecracker-${latest}-${ARCH}.tgz \
 sudo mv release-${latest}-$(uname -m) /usr/local/bin/firecracker
 sudo mv /usr/local/bin/firecracker/firecracker-${latest}-${ARCH} /usr/local/bin/firecracker/firecracker
 sudo sh -c  "echo 'export PATH=\$PATH:/usr/local/bin/firecracker' >> /etc/profile"
+
+# Load systemd files
+sudo cp ~/cluster_manager/scripts/systemd/* /etc/systemd/system/*
