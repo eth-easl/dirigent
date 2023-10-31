d972c729d2c18b5b09405f95b9f960e2afbe1c5f
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2023-10-31 21:27:43 +0100

    Scripts for systemd services
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- scripts/remote_start_cluster.sh
+++ scripts/remote_start_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 source $DIR/common.sh
 
 # Extracting control plane and data plane nodes and worker nodes
@@ -16,23 +16,32 @@ echo "Control plane IP is ${CP_IP_ADDRESS}"
 function SetupControlPlane() {
     RemoteExec $CONTROL_PLANE "cd ~/cluster_manager; git pull"
 
+    # Start Redis server
     RemoteExec $CONTROL_PLANE "docker stop \$(docker ps -aq)"
     RemoteExec $CONTROL_PLANE "docker rm \$(docker ps -a -q)"
     RemoteExec $CONTROL_PLANE "docker run -d --name redis-stack-server -p 6379:6379 redis/redis-stack-server:latest"
 
+    # Compile control plane
+    RemoteExec $CONTROL_PLANE "sudo mkdir -p /cluster_manager/cmd/master_node"
+    RemoteExec $CONTROL_PLANE "cd ~/cluster_manager/cmd/master_node/; /usr/local/go/bin/go build main.go"
+    RemoteExec $CONTROL_PLANE "sudo cp ~/cluster_manager/cmd/master_node/main /cluster_manager/cmd/master_node/"
+    RemoteExec $CONTROL_PLANE "sudo cp ~/cluster_manager/cmd/master_node/config_cluster.yaml /cluster_manager/cmd/master_node/"
+
+    # Start control plane
     RemoteExec $CONTROL_PLANE "sudo systemctl daemon-reload && sudo systemctl start control_plane.service"
 }
 
 function SetupDataPlane() {
     RemoteExec $DATA_PLANE "cd ~/cluster_manager; git pull"
 
-    IP_ADDRESS=$(RemoteExec $DATA_PLANE "netstat -ie | grep -B1 '10.0.1' | sed -n 2p | tr -s ' ' | cut -d ' ' -f 3")
-    RemoteExec $DATA_PLANE 'export DATA_PLANE_IP=\"'$IP_ADDRESS'\"; cd cluster_manager; cat cmd/data_plane/config_cluster.yaml | envsubst > cmd/data_plane/tmp && mv cmd/data_plane/tmp cmd/data_plane/config_cluster.yaml'
+    # Compile data plane
+    RemoteExec $DATA_PLANE "sudo mkdir -p /cluster_manager/cmd/data_plane"
+    RemoteExec $DATA_PLANE "cd ~/cluster_manager/cmd/data_plane/; /usr/local/go/bin/go build main.go"
+    RemoteExec $DATA_PLANE "sudo cp ~/cluster_manager/cmd/data_plane/main /cluster_manager/cmd/data_plane/"
+    RemoteExec $DATA_PLANE "sudo cp ~/cluster_manager/cmd/data_plane/config_cluster.yaml /cluster_manager/cmd/data_plane/"
 
-    RemoteExec $DATA_PLANE "tmux kill-session -t data_plane"
-    RemoteExec $DATA_PLANE "tmux new -s data_plane -d"
-
-    RemoteExec $CONTROL_PLANE "sudo systemctl daemon-reload && sudo systemctl start data_plane.service"
+    # Start data plane
+    RemoteExec $DATA_PLANE "sudo systemctl daemon-reload && sudo systemctl start data_plane.service"
 }
 
 function SetupWorkerNodes() {
@@ -43,9 +52,12 @@ function SetupWorkerNodes() {
         # LFS pull for VM kernel image and rootfs
         RemoteExec $1 "cd ~/cluster_manager; git pull; git lfs pull"
 
-        IP_ADDRESS=$(RemoteExec $1 "netstat -ie | grep -B1 '10.0.1' | sed -n 2p | tr -s ' ' | cut -d ' ' -f 3")
-        RemoteExec $1 'export WORKER_NODE_IP=\"'$IP_ADDRESS'\"; cd cluster_manager; cat cmd/worker_node/config_cluster.yaml | envsubst > cmd/worker_node/tmp && mv cmd/worker_node/tmp cmd/worker_node/config_cluster.yaml'
+        # Compile worker node daemon
+        RemoteExec $1 "sudo mkdir -p /cluster_manager/cmd/worker_node"
+        RemoteExec $1 "cd ~/cluster_manager/cmd/worker_node/; /usr/local/go/bin/go build main.go"
+        RemoteExec $1 "sudo cp -r ~/cluster_manager/* /cluster_manager"
 
+        # Start worker node daemon
         RemoteExec $1 "sudo systemctl daemon-reload && sudo systemctl start worker_node.service"
     }
 
@@ -56,6 +68,24 @@ function SetupWorkerNodes() {
 
     wait
 }
+
+function KillSystemdServices() {
+    function internal_kill() {
+        RemoteExec $1 "sudo systemctl stop worker_node"
+    }
+
+    RemoteExec $CONTROL_PLANE "sudo systemctl stop control_plane"
+    RemoteExec $DATA_PLANE "sudo systemctl stop data_plane"
+
+    for NODE in "$@"
+    do
+        internal_kill $NODE &
+    done
+
+    wait
+}
+
+KillSystemdServices $@
 
 # Starting processes
 SetupControlPlane
