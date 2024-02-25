61f7842f54079f8e81548ac6c7adde27043c25ac
Lazar Cvetković <l.cvetkovic.997@gmail.com>
2024-02-25 22:19:09 +0100

    Keepalived readiness check bugfixing
    Signed-off-by: Lazar Cvetković <l.cvetkovic.997@gmail.com>

--- configs/check_apiserver.sh
+++ configs/check_apiserver.sh
@@ -5,7 +5,7 @@ errorExit() {
     exit 1
 }
 
-curl --silent --max-time 2 --insecure https://localhost:8080/ -o /dev/null || errorExit "Error GET https://localhost:8080/"
-if ip addr | grep -q 10.0.1.8080; then
-    curl --silent --max-time 2 --insecure https://10.0.1.254:8080/ -o /dev/null || errorExit "Error GET https://10.0.1.254:8080/"
+curl --silent --max-time 2 --insecure http://localhost:8080/health -o /dev/null || errorExit "Error GET http://localhost:8080/health"
+if ip addr | grep -q 10.0.1.254; then
+    curl --silent --max-time 2 --insecure http://10.0.1.254:8080/health -o /dev/null || errorExit "Error GET http://10.0.1.254:8080/health"
 fi
