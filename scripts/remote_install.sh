192a86e94760e6f1e57251ee0c1031032d04718c
Luka Simić <git@kocka.tech>
2024-04-28 18:04:34 +0200

    Copy the setup script before running in `remote_install`.

--- scripts/remote_install.sh
+++ scripts/remote_install.sh
@@ -29,6 +29,7 @@ function AddSshKeys() {
 function SetupNode() {
     AddSshKeys $1
     RemoteExec $1 'if [ ! -d ~/cluster_manager ];then git clone --branch=sosp_24_submission git@github.com:eth-easl/cluster_manager.git; fi'
+    CopyToRemote "$DIR/setup_node.sh" "$1:~/cluster_manager/scripts/setup_node.sh"
     RemoteExec $1 "bash ~/cluster_manager/scripts/setup_node.sh $2"
     # LFS pull for VM kernel image and rootfs
     RemoteExec $1 'cd ~/cluster_manager; git pull; git lfs pull'
