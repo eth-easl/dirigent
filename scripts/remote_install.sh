975b6472285b40fad315b273808602e6b4690168
Luka Simić <git@kocka.tech>
2024-04-28 19:10:02 +0200

    Clone the current remote tracking branch in `remote_install`. This also fixes the fact that `sosp_24_submission` no longer exists, and fixes the GitHub repository address. `AddSshKeys` is now replaced by `CloneDirigent`, as it is dependent on whether the repository actually needs to be cloned.

--- scripts/remote_install.sh
+++ scripts/remote_install.sh
@@ -5,37 +5,49 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 source $DIR/common.sh
 source $DIR/setup.cfg
 
+REMOTE_SSH_KEY="~/.ssh/id_ed25519"
+LOCAL_SSH_KEY="$DIR/ssh-dirigent"
+
 function GenerateSSHKeys() {
-    if [ ! -f "$DIR/ssh-dirigent" ]
+    if [ ! -f "$LOCAL_SSH_KEY" ]
     then
-        ssh-keygen -t ed25519 -f "$DIR/ssh-dirigent" -N ''
+        ssh-keygen -t ed25519 -f "$LOCAL_SSH_KEY" -N ''
         echo -e "${color_red}ACTION REQUIRED:${color_white} Please add a new SSH key to your GitHub profile."
         echo -e "${color_green}Step 1:${color_reset} Visit ${color_cyan}https://github.com/settings/ssh/new${color_reset}"
         echo -e "${color_green}Step 2:${color_reset} Paste the following contents:"
-        cat "$DIR/ssh-dirigent.pub"
+        cat "$LOCAL_SSH_KEY.pub"
         read -p "Press Enter after making these changes."
     fi
 }
 
-function AddSshKeys() {
-    exists=$(RemoteExec $1 'if [ -f "~/.ssh/id_rsa" ]; then echo "exists"; fi')
+function CloneDirigent() {
+    if RemoteExec $1 "[ ! -d ~/cluster_manager ]"
+    then
+        # Copy the private SSH key for cloning Dirigent.
+        if RemoteExec $1 "[ ! -f '$REMOTE_SSH_KEY' ]"
+        then
+            RemoteExec $1 'ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts'
+            CopyToRemote "$LOCAL_SSH_KEY" "$1:$REMOTE_SSH_KEY"
+        fi
 
-    if [ "$exists" != "exists" ]; then
-        RemoteExec $1 'ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts'
-        CopyToRemote "$DIR/ssh-dirigent" "$1:~/.ssh/id_rsa"
+        # Clone the current remote tracking branch of Dirigent if there is one,
+        # or default branch if there is not.
+        current_branch=$(git rev-parse --abbrev-ref HEAD)
+        default_branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+        remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} >/dev/null 2>&1 && echo "$current_branch" || echo "$default_branch")
+        RemoteExec $1 "git clone --branch=$remote_branch git@github.com:eth-easl/dirigent.git ~/cluster_manager"
     fi
 }
 
 function SetupNode() {
-    AddSshKeys $1
-    RemoteExec $1 'if [ ! -d ~/cluster_manager ];then git clone --branch=sosp_24_submission git@github.com:eth-easl/cluster_manager.git; fi'
+    CloneDirigent $1
     CopyToRemote "$DIR/setup_node.sh" "$1:~/cluster_manager/scripts/setup_node.sh"
     strict_mode=$([[ "$-" == *e* ]] && echo "bash -e" || echo "bash")
     RemoteExec $1 "$strict_mode ~/cluster_manager/scripts/setup_node.sh $2"
     # LFS pull for VM kernel image and rootfs
     RemoteExec $1 'cd ~/cluster_manager; git pull; git lfs pull'
     RemoteExec $1 'sudo cp -r ~/cluster_manager/ /cluster_manager'
-    RemoteExec $1 'git clone https://github.com/vhive-serverless/invitro'
+    RemoteExec $1 '[ ! -d ~/invitro ] && git clone https://github.com/vhive-serverless/invitro.git'
     CopyToRemote "$DIR/invitro_traces" "$1:invitro"
 }
 
