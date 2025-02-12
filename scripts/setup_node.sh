cd2aa8ee90dc9a7c6a7f5dfa5045cc38874c5987
Luka Simić <git@kocka.tech>
2025-02-12 19:21:58 +0100

    Install GPU drivers up node setup.

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -96,6 +96,19 @@ then
     sudo apt install cargo -y
 fi
 
+# Install GPU drivers
+if sudo lshw -C display | grep -q NVIDIA
+then
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+        sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+        sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sudo apt-get update
+    sudo apt-get install -y ubuntu-drivers-common
+    sudo ubuntu-drivers install --gpgpu
+    sudo apt-get install -y nvidia-container-toolkit
+fi
+
 # Copy systemd services
 sudo cp -a ~/cluster_manager/scripts/systemd/* /etc/systemd/system/
 
